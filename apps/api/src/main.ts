aa71a05818c67f378cbd051f733538018f1d9e51
harshalyuvraj <hrslyuvi@gmail.com>
2026-08-12 22:51:22 +0530

    Fix production CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: ['http://localhost:3000', 'https://notifyx-web.onrender.com'],
     credentials: true,
   });
 
